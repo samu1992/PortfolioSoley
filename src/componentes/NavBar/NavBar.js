a2444dbfd56d45538ac435809a2e143f26a5858c
samuel carrizo <samuelcarrizot@gmail.com>
2023-01-26 11:29:42 -0300

    se cambio la ruta del resume

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import './NavBar.css'
 import logo from './LOGO.svg';
 import { Link } from 'react-router-dom';
+import resume from './soleyflores.pdf'
 
 
 const NavBar = () => {
@@ -10,14 +11,14 @@ return (
         <div className='container_nav--links'>
             <ul>
                 <li><Link to='/About'>About</Link></li>
-                <li><a target='_blank' rel='noreferrer' href='https://drive.google.com/file/d/1o1LfwlJqLc9MGZOUiGuH__0fuJmxkCwQ/view?usp=sharing'>Resume</a></li>
+                <li><a target='_blank' rel='noreferrer' href={resume}>Resume</a></li>
                 <li><a target="_blank" rel='noreferrer' href='http://www.linkedin.com/in/soleyflores'>LinkedIn</a></li>
             </ul>
         </div>
         <Link to='#' class="fa-solid fa-grip-lines lines"/>
         <div className='menu_responsive' id='responsive'>
                 <Link  to='/About'>About</Link>
-                <a href='.'>Resume</a>
+                <a target='_blank' rel='noreferrer' href={resume}>Resume</a>
                 <a target="_blank" rel='noreferrer' href='http://www.linkedin.com/in/soleyflores'>LinkedIn</a>
             </div>
     </header>
